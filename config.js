d284c1103045a46d49d30eabd607ea247f898b5b
InconuDuWeb <167088691+InconuDuWeb@users.noreply.github.com>
2024-05-08 12:03:47 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+        app: {
+            px: '+',
+            token: 'TOKEN_BOT',
+            owners: 'TON_ID',
+            funny: 'TON_ID',
+            color: '#FF0000',
+            footer: 'Made by Zedrox_dsc',
+            maxserver: '5',
+            maxVol: '150',
+            everyoneMention: false,
+            hostedBy: true,
+            discordPlayer: {
+                ytdlOptions: {
+                    quality: 'highestaudio',
+                    highWaterMark: 1 << 25
+                }
+            }
+        }
+    }
